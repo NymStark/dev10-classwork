81ce7a9dfbee884f8d03a2d876006515017be744
Hau Nguyen <hau263@revature.com>
2023-08-11 13:09:46 -0400

    wrapping up things

--- week-01/exercises/arrays-exercises/src/Exercise16.java
+++ week-01/exercises/arrays-exercises/src/Exercise16.java
@@ -1,7 +1,20 @@
 import java.util.Random;
 
 public class Exercise16 {
+    // makeRandomAscendingArray creates a random array with a capacity between 50 and 150.
+    // Its elements are guaranteed to be sorted ascending.
+    // 1. Create a new int[] with capacity for all elements from `one` and `two`.
+    // 2. "Merge" elements from `one` and `two` into the new array so that its values are sorted.
 
+         /* Pseudocode:
+         Create an integer index for `one`, `two`, and the result array, all starting at 0.
+         Loop resultIndex from 0 to result.length - 1:
+           if one[oneIndex] is less than two[twoIndex], add it to the result and increment oneIndex.
+           if two[twoIndex] is less than one[oneIndex], add it to the result and increment twoIndex.
+           determine how to settle ties
+           if oneIndex >= one.length, there are no `one` elements remaining so use elements from two
+           if twoIndex >= two.length, there are no `two` elements remaining so use elements from one
+          */
     public static void main(String[] args) {
         // MERGE
         int[] one = makeRandomAscendingArray(); //first random ascending array
@@ -15,10 +28,10 @@ public class Exercise16 {
         int twoIndex = 0;
         int resultIndex = 0;
         while (resultIndex < result.length) {
-            if (oneIndex<lenOne && ( twoIndex >= lenTwo || one[oneIndex]<= two[twoIndex])){ // iterates through one, while one[] is smaller than two[]
+            if (oneIndex<lenOne && ( twoIndex >= lenTwo || one[oneIndex]<= two[twoIndex])){
                 result[resultIndex] = one[oneIndex];
                 oneIndex++;
-            } else if ( twoIndex<lenTwo) { // iterates through two.
+            } else if ( twoIndex<lenTwo) {
                 result[resultIndex] = two[twoIndex];
                 twoIndex++;
             }
@@ -41,20 +54,7 @@ public class Exercise16 {
             System.out.print(num + "->");
         }
 
-        // makeRandomAscendingArray creates a random array with a capacity between 50 and 150.
-        // Its elements are guaranteed to be sorted ascending.
-        // 1. Create a new int[] with capacity for all elements from `one` and `two`.
-        // 2. "Merge" elements from `one` and `two` into the new array so that its values are sorted.
 
-         /* Pseudocode:
-         Create an integer index for `one`, `two`, and the result array, all starting at 0.
-         Loop resultIndex from 0 to result.length - 1:
-           if one[oneIndex] is less than two[twoIndex], add it to the result and increment oneIndex.
-           if two[twoIndex] is less than one[oneIndex], add it to the result and increment twoIndex.
-           determine how to settle ties
-           if oneIndex >= one.length, there are no `one` elements remaining so use elements from two
-           if twoIndex >= two.length, there are no `two` elements remaining so use elements from one
-          */
     }
 
     public static int[] makeRandomAscendingArray() {
