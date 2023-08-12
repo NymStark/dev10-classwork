e9b01e558ff12d685e8471f6cf411f2d24ab61c5
Hau Nguyen <hau263@revature.com>
2023-08-11 20:46:31 -0400

    Take one

---
+++ week-01/assessment/capsule-hotel-project/HanoiCapsuleManagement.java
@@ -0,0 +1,198 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.function.DoubleFunction;
+
+/*
+    1. The goal is to create a hotel capsule management
+    2. Data structure will be simple string array.
+    3. Reusable methods to help with functionality:
+    initializeCapsules - populate the capsule with "Vacant"
+    check-in: Our checking in method
+    check-out: Our checking out method
+    displayOccupancy: Occupancy display method
+    confirmExit: Our exit options confirmation
+ */
+public class HanoiCapsuleManagement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Welcome msgs
+        System.out.println("===========================");
+        System.out.println("Welcome to Hanoi Capsules!!");
+        System.out.println("===========================");
+        System.out.print("Enter the amount of capsules available: ");
+        int numberOfCapsules = Integer.parseInt(scanner.nextLine());
+
+        String[] capsules = new String[numberOfCapsules];
+        initializecapsules(capsules);
+        boolean exit = false; //exit condition
+        while (!exit) {
+            displayMenu();
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch(choice) {
+                case 1:
+                    checkIn(capsules, scanner);
+                    break;
+                case 2:
+                    checkOut(capsules, scanner);
+                    break;
+                case 3:
+                    displayOccupancy(capsules, scanner);
+                    break;
+                case 4:
+                    if (confirmExit(scanner)) {
+                        exit = true;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option");
+            }
+        }
+
+    }
+
+    public static void initializecapsules(String[] capsules){
+        for (int i=0; i < capsules.length; i++){
+            capsules[i] = "Vacant";
+        }
+    }
+
+    public static void displayMenu() {
+        System.out.println("==============");
+        System.out.println("**Guest Menu**");
+        System.out.println("==============");
+        System.out.println("1. Checking In");
+        System.out.println("2. Checking Out");
+        System.out.println("3. Display Occupancy");
+        System.out.println("4. Exit");
+        System.out.println("Select an option [1-4]: ");
+    }
+
+    public static void checkIn(String[] capsules, Scanner scanner){
+        System.out.println("===============");
+        System.out.println("**Checking in**");
+        System.out.println("===============");
+        //this method checks for the number of empty room and displays it.
+        //if there are no available rooms, exit. "We're sorry but our capsules are fully occupied"
+        //Prompts the user for a name:
+        //Prompts the user for a room preference:
+        //If the room is available. update the name to the correct position
+        //If the room is unavailable ask them to put in another room until an available room is found.
+
+        int availableRooms = 0;
+        for (String capsule: capsules) {
+            if (capsule.equals("Vacant")){
+                availableRooms++;
+            }
+        }
+        if (availableRooms==0){
+            System.out.println("We are sorry, but our capsules are fully occupied right now.");
+            return;
+        }
+        System.out.println("The Number of available rooms is: " + availableRooms);
+        System.out.println("Enter guest name: ");
+        String guestName = scanner.nextLine();
+
+        int preferredRoom;
+        boolean validRoom;
+
+        do {
+            System.out.println("Enter the room the guest would prefer: ");
+            preferredRoom = Integer.parseInt(scanner.nextLine()) - 1; //Adjusting index
+            validRoom = preferredRoom >= 0 && preferredRoom < capsules.length && capsules[preferredRoom].equals("Vacant");
+
+            if(!validRoom){
+                System.out.println("Not available.");
+            }
+
+        } while (!validRoom);
+
+        capsules[preferredRoom] = guestName;
+        System.out.println("Check-in Successful! ");
+
+    }
+
+    public static void checkOut(String[] capsules, Scanner scanner){
+        System.out.println("================");
+        System.out.println("**Checking Out**");
+        System.out.println("================");
+//        - Enter Room Number:
+//        - Invalid number check
+//        - Vacancy check
+//        - Else successful checkout after switching capsules[i] back to vacant.
+
+        System.out.println("Please enter the room you're checking out from: ");
+        int checkOutRoom = Integer.parseInt(scanner.nextLine()) -1;
+
+        if (checkOutRoom < 0 || checkOutRoom >= capsules.length) {
+            System.out.println("Invalid room number. Please choose a valid room number.");
+            return;
+        }
+
+        if (!capsules[checkOutRoom].equals("Vacant")) {
+            String temp = capsules[checkOutRoom];
+            capsules[checkOutRoom] = "Vacant";
+            System.out.println("Successfully checked " + temp + " out from room " + checkOutRoom + ".");
+        } else {
+            System.out.println("Cannot check out, the room is already vacant.");
+        }
+    }
+
+    public static void displayOccupancy(String[] capsules, Scanner scanner) {
+        System.out.println("========================");
+        System.out.println("**Displaying Occupancy**");
+        System.out.println("========================");
+        if (capsules.length <= 11) {
+            System.out.println("Room Occupancy:");
+            for (int i = 0; i < capsules.length; i++) {
+                System.out.println("Room " + (i + 1) + ": " + (capsules[i].equals("Vacant") ? "Vacant" : "Occupied by " + capsules[i]));
+                //compares capsules[i] to "Vacant", option 1 returns "Vacant", option 2 returns Occupied by name of guest!
+            }
+        } else {
+            System.out.print("Enter the room number: ");
+            int roomNumber = Integer.parseInt(scanner.nextLine()) - 1; // Adjusting index
+
+            if (roomNumber < 0 || roomNumber >= capsules.length) {
+                System.out.println("Invalid room number. Please choose a valid room number.");
+                return;
+            }
+
+            int startIndex = Math.max(0, roomNumber - 5); // Starting index for display
+            int endIndex = Math.min(capsules.length - 1, roomNumber + 5); // Ending index for display
+
+            System.out.println("Room Occupancy:");
+            for (int i = startIndex; i <= endIndex; i++) {
+                System.out.println("Room " + (i + 1) + ": " + (capsules[i].equals("Vacant") ? "Vacant" : "Occupied by " + capsules[i]));
+            }
+        }
+
+    }
+
+
+    public static boolean confirmExit(Scanner scanner) {
+        // Confirm exit method, that takes the input argument
+        // Basic input validation to only Y or N, takes both capitalization and lowercase.
+        // true if y, false if n.
+        System.out.println("===================");
+        System.out.println("**Confirming Exit**");
+        System.out.println("===================");
+        System.out.println("Are you sure you want to exit?(y/n): ");
+        char input = scanner.next().toLowerCase().charAt(0);
+        while (input != 'y' && input !='n' ) {
+            System.out.println("Invalid input. Please enter 'y' or 'n'. ");
+            System.out.println("Are you sure you want to exit?");
+            input = scanner.next().toLowerCase().charAt(0);
+        }
+
+        if (input == 'y'){
+            System.out.println("Thank you for using Hanoi Capsule Management System!!");
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
