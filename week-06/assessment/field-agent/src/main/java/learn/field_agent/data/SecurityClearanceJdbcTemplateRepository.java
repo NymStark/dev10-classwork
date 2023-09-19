8cc2b3448024a96dbbe978a1b7a85732da76fb3e
HauOnMLaptop <hau.q.nguyen@gmail.com>
2023-09-19 07:54:52 -0400

    committing from MD

--- week-06/assessment/field-agent/src/main/java/learn/field_agent/data/SecurityClearanceJdbcTemplateRepository.java
+++ week-06/assessment/field-agent/src/main/java/learn/field_agent/data/SecurityClearanceJdbcTemplateRepository.java
@@ -3,7 +3,13 @@ package learn.field_agent.data;
 import learn.field_agent.data.mappers.SecurityClearanceMapper;
 import learn.field_agent.models.SecurityClearance;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
 
 @Repository
 public class SecurityClearanceJdbcTemplateRepository implements SecurityClearanceRepository {
@@ -25,4 +31,52 @@ public class SecurityClearanceJdbcTemplateRepository implements SecurityClearanc
                 .stream()
                 .findFirst().orElse(null);
     }
+    public SecurityClearance findByName(String name) {
+        final String sql = "SELECT security_clearance_id, name security_clearance_name FROM security_clearance WHERE name = ?;";
+        return jdbcTemplate.query(sql, new SecurityClearanceMapper(), name).stream()
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<SecurityClearance> findAll() {
+        final String sql = "SELECT security_clearance_id, name security_clearance_name FROM security_clearance limit 1000";
+        return jdbcTemplate.query(sql, new SecurityClearanceMapper());
+    }
+
+    @Override
+    public SecurityClearance add(SecurityClearance securityClearance) {
+        final String sql = "INSERT INTO security_clearance (`name`) VALUES (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        // literally just following convention of the code in the project,
+
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, securityClearance.getName());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        securityClearance.setSecurityClearanceId(keyHolder.getKey().intValue());
+        return securityClearance;
+    }
+
+    @Override
+    public boolean update(SecurityClearance securityClearance) {
+        final String sql = "UPDATE security_clearance SET `name` = ? WHERE security_clearance_id = ?";
+        return jdbcTemplate.update(sql, securityClearance.getName(), securityClearance.getSecurityClearanceId()) > 0;
+    }
+
+    @Override
+    public boolean deleteById(int securityClearanceId) {
+        final String deleteAgentSecurityClearance = "DELETE FROM agency_agent WHERE security_clearance_id = ?";
+        jdbcTemplate.update(deleteAgentSecurityClearance, securityClearanceId);
+
+        final String sql = "DELETE FROM security_clearance WHERE security_clearance_id = ?";
+        return jdbcTemplate.update(sql, securityClearanceId) > 0;
+    }
+
 }
